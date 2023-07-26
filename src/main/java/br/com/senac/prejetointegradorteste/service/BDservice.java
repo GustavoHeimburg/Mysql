940e0a8896c8543d2e5c698e6a1f74fd36b77462
Gustavo Heimburg Pereira <gutogustavogu@gmail.com>
2023-07-26 12:28:06 -0300

    Atualização BD

--- src/main/java/br/com/senac/prejetointegradorteste/service/BDservice.java
+++ src/main/java/br/com/senac/prejetointegradorteste/service/BDservice.java
@@ -14,14 +14,15 @@ public class BDservice {
             conexao = DriverManager.getConnection("jdbc:mysql://localhost/senac_projeto", "root", "root");
             Statement stmt = conexao.createStatement();
 
-            String nome = "";
-            String senha = "";
+            String nome = "Giovane";
+            String senha = "Pop232";
             String sql = "INSERT INTO usuarios (nome, senha) VALUES ('" + nome + "', '" + senha + "')";
             stmt.executeUpdate(sql);
 
             ResultSet rsUsuarios = stmt.executeQuery("SELECT * FROM usuarios");
             while (rsUsuarios.next()) {
                 System.out.println("nome: " + rsUsuarios.getString("nome"));
+                System.out.println("senha: " + rsUsuarios.getString("senha"));
             }
         } catch (ClassNotFoundException ex) {
             System.out.println("O driver de conexao nao foi encontado");
